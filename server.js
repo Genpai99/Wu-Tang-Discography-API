07dd4f0d3edfa3bbe410d06be74d598f297f8ce5
genpai99 <darioussanchez@gmail.com>
2023-06-08 02:03:42 -0400

    yeah

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const axios = require('axios');
+const path = require('path');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = express();
